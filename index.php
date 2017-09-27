c8795e54fa33be9ea6b90f8c2af3fbcc454ad5e3
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-09-27 15:38:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <form action="process_form.php" method="get" autocomplete="on">
   First name:<input type="text" name="fname"><br>
   E-mail: <input type="email" name="email"><br>
+  Mobile: <input type="text" name="mobile"><br>
   <input type="submit">
 </form>
 
