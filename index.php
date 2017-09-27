c308b2bf514e10e70f53198e6fcf3067f4bef03a
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2017-09-27 15:40:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <p>This is a test application to check if AUTO FILL is working</p>
 <br>
   <br>
-<form action="process_form.php" method="get" autocomplete="on">
+<form action="process_form.php" method="post" autocomplete="on">
   First name:<input type="text" name="fname"><br>
   Email: <input type="email" name="email"><br>
   Mobile: <input type="text" name="mobile"><br>
